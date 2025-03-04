a1495fc731e3324f5e1a764b8184e139df788ca0
Guilherme Nicolini <guilherme.nicolini@kenlo.com.br>
2025-03-04 19:17:01 -0300

    feat: add ahgora post

---
+++ src/app/action.js
@@ -0,0 +1,37 @@
+"use server";
+
+export async function add(formData) {
+  const timestamp = new Date().valueOf();
+  const accuracy =
+    Math.floor(Math.random() * 5010000000000 + 5000000000000) / 10000000000;
+
+  const response = await fetch(process.env.AHGORA_URL, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      account: process.env.AHGORA_ACCOUNT,
+      accuracy: accuracy,
+      identification_type: "matricula_senha",
+      identity: process.env.AHGORA_MACHINECODE,
+      latitude: process.env.AHGORA_LATITUDE,
+      longitude: process.env.AHGORA_LONGITUDE,
+      logon: false,
+      offline: false,
+      origin: "chr",
+      password: process.env.AHGORA_PASSWORD,
+      provider: "network/wifi",
+      timestamp: timestamp,
+      timestamp_loc: timestamp,
+      version: process.env.AHGORA_VERSION ?? "1.1.2",
+    }),
+  });
+
+  const json = await response.json();
+
+  return {
+    ok: json.result,
+    message: json.result ? "Batida adicionada com sucesso" : json.reason,
+  };
+}
--- src/app/page.js
+++ src/app/page.js
@@ -1,95 +1,71 @@
+"use client";
 import Image from "next/image";
 import styles from "./page.module.css";
+import { add } from "./action";
+import { useState } from "react";
 
 export default function Home() {
+  const [data, setData] = useState({
+    message: "",
+    items: [],
+  });
+
+  const handleSubmit = async (formData) => {
+    setData({ ...data, message: "" });
+    const result = await add(formData);
+
+    setData({ ...data, message: result.message });
+
+    if (result.ok) {
+      const now = new Date();
+      setData({
+        ...data,
+        items: [
+          ...data.items,
+          {
+            key: now.valueOf(),
+            value: `${now.getDate().toString().padStart(2, "0")}/${(
+              now.getMonth() + 1
+            )
+              .toString()
+              .padStart(2, "0")}/${now.getFullYear()} ${now
+              .getHours()
+              .toString()
+              .padStart(2, "0")}:${now
+              .getMinutes()
+              .toString()
+              .padStart(2, "0")}`,
+          },
+        ],
+      });
+    }
+  };
+
   return (
     <div className={styles.page}>
       <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
+        Últimas batidas
         <ol>
-          <li>
-            Get started by editing <code>src/app/page.js</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
+          {data.items.map((item) => (
+            <li key={item.key}>{item.value}</li>
+          ))}
         </ol>
-
         <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
+          <form action={handleSubmit} noValidate>
+            <button className={styles.primary}>
+              <Image
+                className={styles.logo}
+                src="/vercel.svg"
+                alt="Vercel logomark"
+                width={20}
+                height={20}
+              />
+              Bater Ponto
+            </button>
+            <p aria-live="polite">{data?.message}</p>
+          </form>
         </div>
       </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
     </div>
   );
 }
